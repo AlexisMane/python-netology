cb3df490ccd56bf9b35ca13cadf74cb9979e1593
AlexisMane <ewerewerewer@gmail.com>
2017-12-24 21:34:06 +0300

    Added 2.4

---
+++ 2.4-Paths/2.4.py
@@ -0,0 +1,25 @@
+# coding=Windows_1251
+import os
+
+migrations = 'Migrations'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+def searching():
+    search = input('¬ведите строку:')
+    counter = int()
+    for f in sql_list.copy():
+        if f.endswith(".sql"):
+            if search in f:
+                print(f)
+                counter += 1
+            else:
+                sql_list.remove(f)
+        else:
+            sql_list.remove(f)
+    print(counter)
+
+
+if __name__ == '__main__':
+    sql_list = os.listdir(migrations)
+    while True:
+        searching()
